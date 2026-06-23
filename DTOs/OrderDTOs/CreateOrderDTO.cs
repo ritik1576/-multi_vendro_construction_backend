f75b1477411cf08f9db3c7d76cc79d1ac48c1db9
Dev Sonwani <dev.sonwani.averybit@GMAIL.COM>
2026-06-23 12:48:57 +0530

    fixed serialization error

--- DTOs/OrderDTOs/CreateOrderDTO.cs
+++ DTOs/OrderDTOs/CreateOrderDTO.cs
@@ -1,12 +1,29 @@
+using System.Text.Json.Serialization;
+
 public class CreateOrderDto
 {
     public long UserId { get; set; }
 
+    [JsonPropertyName("user_id")]
+    public long UserIdSnakeCase { get => UserId; set => UserId = value; }
+
     public long AddressId { get; set; }
+
+    [JsonPropertyName("address_id")]
+    public long AddressIdSnakeCase { get => AddressId; set => AddressId = value; }
 
     public string? CouponCode { get; set; }
 
+    [JsonPropertyName("coupon_code")]
+    public string? CouponCodeSnakeCase { get => CouponCode; set => CouponCode = value; }
+
     public string? PaymentMethod { get; set; } = "razorpay";
 
+    [JsonPropertyName("payment_method")]
+    public string? PaymentMethodSnakeCase { get => PaymentMethod; set => PaymentMethod = value; }
+
     public List<CreateOrderItemDto> Items { get; set; } = new();
+
+    [JsonPropertyName("items")]
+    public List<CreateOrderItemDto> ItemsSnakeCase { get => Items; set => Items = value; }
 }
